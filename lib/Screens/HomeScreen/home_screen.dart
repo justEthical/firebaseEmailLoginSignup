69a601a5c5d6e3772e4f8f2efa5d69b6bec92708
Tyson <ammmm4507@gmail.com>
2022-02-08 09:44:39 +0530

    added User email and signout button

--- lib/Screens/HomeScreen/home_screen.dart
+++ lib/Screens/HomeScreen/home_screen.dart
@@ -1,18 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  User? usr = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("HomeScreen"),),
-      
+      appBar: AppBar(
+        title: Text("HomeScreen"),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "${usr!.email}",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 50,
+              height: 90,
+              padding: const EdgeInsets.all(20),
+              child: ElevatedButton(
+                child: const Text(
+                  "SignOut",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
